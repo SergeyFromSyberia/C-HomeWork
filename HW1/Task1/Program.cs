1fe1963cf382e85e7de8d66b362aa96fd3a8d149
Sergey <srjtarkan@mail.ru>
2022-08-22 21:01:34 +0700

    закончил первое задание

--- HW1/Task1/Program.cs
+++ HW1/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите первое число:");
-int num1 = int.Parse (Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
-int num2 = int.Parse (Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 > num2)
 {
     Console.WriteLine($"Максимальное число {num1}");
