2bea867d3b30b6cf45a160d49fc439273689abd0
Sergey <srjtarkan@mail.ru>
2022-08-24 00:05:26 +0700

    Закончил четвертое задание

---
+++ HW1/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (num % 2 == 0)
+{
+    while (count < num)
+    {
+        if (count % 2 == 0) Console.Write($"{count},");
+        count++;
+    }
+    Console.WriteLine($"{count}.");
+}
+else if (num % 2 != 0)
+{
+    while (count < num-1)
+    {
+        if (count % 2 == 0) Console.Write($"{count},");
+        count++;
+    }
+    Console.WriteLine($"{count}.");
+}
