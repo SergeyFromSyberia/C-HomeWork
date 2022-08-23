c0b2e252267fe467fe4eb68a581ff944e5dfc78f
Sergey <srjtarkan@mail.ru>
2022-08-23 23:03:50 +0700

    закончил третье задание

---
+++ HW1/Task3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0) System.Console.WriteLine($"Число {num} является чётным");
+else System.Console.WriteLine($"Число {num} не является чётным");
